4e466a3718192a253cfcc536315a1135cd1a25f2
Your Name <you@example.com>
2023-02-07 17:18:17 +0700

    done

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,21 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
-	
-	
+	int a = 5 ;
+    string b = "A" ; 
+    string &c = b ;
+    int *x = &a , **z = &x ;
+	string *y = &b ;
+    cout << a <<" " << b <<" " << c << " " <<  &x << " " << &y << " " << &z << "\n";
+	cout << &a <<" " << &b <<" " << &c << " " <<  &x << " " << &y << " " << &z << "\n" ;
+	c = "F" ;
+	cout << a <<" " << b <<" " << c << " " <<  &x << " " << &y << " " << &z << "\n" ;
+	*y = "W" ;
+	cout << a <<" " << b <<" " << c << " " <<  &x << " " << &y << " " << &z << "\n" ;
+	*x = 6 ;
+	cout << a <<" " << b <<" " << c << " " <<  &x << " " << &y << " " << &z << "\n" ;
+	**z = 7 ;
+	cout << a <<" " << b <<" " << c << " " <<  &x << " " << &y << " " << &z << "\n" ;
 	return 0;
 }
--- lab16_5.cpp
+++ lab16_5.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 using namespace std;
 
-void shuffle(int &,int &, int &, int &); //Modify input arguments to pointer 
+void shuffle(int *,int *, int *, int *); //Modify input arguments to pointer 
 
 int main(){
 	int a = 50, b = 100, c = 500, d = 1000;
@@ -11,11 +11,26 @@ int main(){
 	srand(time(0));	
 	
 	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d); //Modify input arguments to pointer 
+	    shuffle(&a,&b,&c,&d); //Modify input arguments to pointer 
 	    cout << a << " " << b << " " << c << " " << d << "\n";
 	}
 	
 	return 0;
 }
 
-//Write definition of shuffle() using pointer here 
+void shuffle(int *a,int *b,int *c,int *d){
+    int x = rand()%2;
+    if(x==0){
+    int z = *a;
+    *a=*b;
+    *b=*c;
+    *c=*d;
+    *d=z;
+    }
+    if(x==1)
+    {
+    int z = *b;
+    int y = *c;
+    *b=*d; *c =*a ; *a = y; *d = z;
+    }
+    }
